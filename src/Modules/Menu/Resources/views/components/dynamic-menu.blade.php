8b3ceb437be2e7232d6580468300eabc88c790a5
Nasir Khan Saikat <nasir8891@gmail.com>
2026-02-28 02:33:29 +0600

    refactor: update CSS classes for dynamic menu and enhance dropdown item structure

--- src/Modules/Menu/Resources/views/components/dynamic-menu.blade.php
+++ src/Modules/Menu/Resources/views/components/dynamic-menu.blade.php
@@ -1,4 +1,4 @@
-@props(['location', 'cssClass' => 'flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-8', 'itemComponent' => null])
+@props(['location', 'cssClass' => 'flex flex-col sm:flex-row space-y-2 md:flex-row md:space-y-0 md:space-x-8', 'itemComponent' => null])
 
 @php
     use Nasirkhan\ModuleManager\Modules\Menu\Models\Menu;
--- src/Modules/Menu/Resources/views/components/menu-item.blade.php
+++ src/Modules/Menu/Resources/views/components/menu-item.blade.php
@@ -36,7 +36,7 @@
     
     @case('dropdown')
         @if($hasChildren)
-            <li class="relative group">
+            <li class="relative group ">
                 <button
                     type="button"
                     class="border-transparent dark:border-transparent cursor-pointer flex items-center justify-between w-full md:w-auto border-b-2 px-3 py-2 font-semibold text-gray-800 transition duration-200 ease-in hover:border-gray-700 hover:opacity-75 dark:text-white dark:hover:border-gray-300 dark:hover:opacity-75 sm:my-0 sm:py-1"
@@ -45,9 +45,9 @@
                     aria-haspopup="true"
                     aria-label="Toggle {{ $item->getDisplayTitle() }} submenu"
                 >
-                    {{ $item->getDisplayTitle() }}
-                    <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                    <span>{{ $item->getDisplayTitle() }}</span>
+                    <svg class="w-6 h-6 ml-2.5 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 10 4 4 4-4"/>
                     </svg>
                 </button>
 
