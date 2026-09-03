079ee6515627b58825b58dc7d689e9453e76c074
Nasir Khan Saikat <nasir8891@gmail.com>
2026-09-03 11:19:25 +0600

    fix(routes): adjust indentation for clarity and add restore route for trashed posts

--- src/Modules/Post/routes/web.php
+++ src/Modules/Post/routes/web.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 *
 * --------------------------------------------------------------------
 */
+
 Route::group(['namespace' => '\Nasirkhan\ModuleManager\Modules\Post\Http\Controllers\Frontend', 'as' => 'frontend.', 'middleware' => 'web', 'prefix' => ''], function () {
     /*
      *
@@ -55,7 +56,7 @@ Route::group(['namespace' => '\Nasirkhan\ModuleManager\Modules\Post\Http\Control
      */
     $module_name = 'posts';
     $controller_name = 'PostsController';
-Route::get("$module_name/trashed", ['as' => "$module_name.trashed", 'uses' => "$controller_name@trashed"]);
+    Route::get("$module_name/trashed", ['as' => "$module_name.trashed", 'uses' => "$controller_name@trashed"]);
     Route::patch("$module_name/trashed/{id}", ['as' => "$module_name.restore", 'uses' => "$controller_name@restore"]);
     Route::resource("$module_name", "$controller_name");
 });
